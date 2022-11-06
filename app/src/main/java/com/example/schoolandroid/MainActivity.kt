344825ae326452b26953a373aebc88adcd0c8228
kiosura <samotokhin.art@yandex.ru>
2022-11-06 21:38:05 +0300

    -actionbar

--- app/src/main/java/com/example/schoolandroid/MainActivity.kt
+++ app/src/main/java/com/example/schoolandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.schoolandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import androidx.fragment.app.Fragment
 import com.example.schoolandroid.adapter.PageAdapter
 import com.example.schoolandroid.databinding.ActivityMainBinding
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
