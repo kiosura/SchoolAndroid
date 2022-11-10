512b0b3f612216214f22ef3f28751f0076573da8
kiosura <samotokhin.art@yandex.ru>
2022-11-10 18:40:17 +0300

    same as previos whit chats, but chats dont work idn why

--- app/src/main/java/com/example/schoolandroid/screens/course/chats.kt
+++ app/src/main/java/com/example/schoolandroid/screens/course/chats.kt
@@ -5,17 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schoolandroid.R
 import com.example.schoolandroid.adapter.recycleview.ChatsAdapter
 import com.example.schoolandroid.data.Chat
+import java.text.FieldPosition
 
 
-class chats : Fragment() {
+class chats : Fragment(), ChatsAdapter.Listener {
 
     private lateinit var recycleView: RecyclerView
-    val chats_adapter = ChatsAdapter()
+    val chats_adapter = ChatsAdapter(/*this*/)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,5 +49,10 @@ class chats : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = chats()
+    }
+
+    override fun onClick(position : Int) {
+        //Toast.makeText(context,"Clicked on chat № ${position}" ,Toast.LENGTH_LONG).show()
+        println("34iuo5hrlkjewhtlkh34o2y7583476583746583746857345")
     }
 }
