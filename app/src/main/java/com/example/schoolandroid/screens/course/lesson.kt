47d1fb2d545d06028b411a4ba368bebbd3ebaa12
fluxx1on <nick.kaliga@ya.ru>
2022-11-14 02:45:23 +0300

    buttons in lesson fragment is active now

--- app/src/main/java/com/example/schoolandroid/screens/course/lesson.kt
+++ app/src/main/java/com/example/schoolandroid/screens/course/lesson.kt
@@ -1,46 +1,69 @@
 package com.example.schoolandroid.screens.course
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.FrameLayout
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.example.schoolandroid.R
+import com.example.schoolandroid.adapter.recycleview.FilesAdapter
 import com.example.schoolandroid.adapter.recycleview.TaskAdapter
+import com.example.schoolandroid.data.File
 import com.example.schoolandroid.data.Task
 import com.example.schoolandroid.interfaces.Listener
 import com.example.schoolandroid.screens.BaseFragment
-import com.google.android.material.tabs.TabLayout
 
 
 class lesson : BaseFragment(R.layout.fragment_lesson), Listener {
 
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewFiles: RecyclerView
+    private lateinit var recyclerViewTasks: RecyclerView
     private val task_adapter : TaskAdapter = TaskAdapter(this, R.layout.task_card_view)
+    private val file_adapter : FilesAdapter = FilesAdapter()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val lessonPlan = view.findViewById<LinearLayout>(R.id.lessonPlan)
+        lessonPlan.isVisible = false
 
-        recyclerView = view.findViewById(R.id.recycleTasks)
-        with(recyclerView) {
-            //setHasFixedSize(true)
+        recyclerViewFiles = view.findViewById(R.id.filesRecycler)
+        with(recyclerViewFiles) {
+            layoutManager = LinearLayoutManager(view.context)
+            adapter = file_adapter
+        }
+
+        for (i in 1..3){
+            file_adapter.addFile(File("file$i"))
+        }
+        recyclerViewFiles.isVisible = false
+
+        view.findViewById<Button>(R.id.filesButton)
+            .setOnClickListener {
+                recyclerViewFiles.isVisible = !recyclerViewFiles.isVisible
+            }
+
+        view.findViewById<Button>(R.id.lessonPlanButton)
+            .setOnClickListener {
+                lessonPlan.isVisible = !lessonPlan.isVisible
+            }
+
+        recyclerViewTasks = view.findViewById(R.id.recycleTasks)
+        with(recyclerViewTasks) {
             layoutManager = GridLayoutManager(view.context, 4)
             adapter = task_adapter
-            //suppressLayout(true)
         }
 
         for (i in 1..40){
             task_adapter.addTask(Task(i, " hui", resources.getColor(R.color.purple_500)))
+        }
+
+        view.findViewById<Button>(R.id.progress).setOnClickListener{
+            onClick(0)
         }
     }
 
