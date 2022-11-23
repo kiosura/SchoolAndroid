248baa184b8980e147d9a6fcf240d886e3d2aa6a
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2022-11-24 01:40:11 +0300

    about_course fragment modified, binding for textviews added

--- app/src/main/java/com/example/schoolandroid/screens/course/about_course.kt
+++ app/src/main/java/com/example/schoolandroid/screens/course/about_course.kt
@@ -3,6 +3,7 @@ package com.example.schoolandroid.screens.course
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.schoolandroid.R
 import com.example.schoolandroid.adapter.recycleview.AboutCourseLessonsAdapter
 import com.example.schoolandroid.api.CourseViewModel
+import com.example.schoolandroid.data.CourseItem
+import com.example.schoolandroid.databinding.FragmentAboutCourseBinding
 //import com.example.schoolandroid.api.FirstApi.Companion.apiBase
 import com.example.schoolandroid.interfaces.Listener
 import com.example.schoolandroid.screens.BaseFragment
@@ -19,7 +22,6 @@ import kotlinx.coroutines.launch
 
 
 class about_course : BaseFragment(R.layout.fragment_about_course), Listener {
-    private lateinit var recycleView: RecyclerView
     private val lessonsAdapter : AboutCourseLessonsAdapter = AboutCourseLessonsAdapter(this)
 
     private lateinit var CourseVM : CourseViewModel
@@ -30,11 +32,25 @@ class about_course : BaseFragment(R.layout.fragment_about_course), Listener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recycleView = view.findViewById(R.id.recycleLessons)
-        with(recycleView) {
-            layoutManager = LinearLayoutManager(view.context)
-            adapter = lessonsAdapter
+
+        val binding = FragmentAboutCourseBinding.bind(view)
+        fun bind() = with(binding) {
+            val currentCourse = Storage.getCurrentCourse()
+            currentCourse.observe(viewLifecycleOwner) { list ->
+                fun makeTeacherName(list : CourseItem) : String {
+                    return list.teachers[0].name + " " + list.teachers[0].surname
+                }
+                courseName.text = list.name
+                courseDescription.text = list.product_preview
+                courseTutor.text = makeTeacherName(list)
+                with(recycleLessons) {
+                    layoutManager = LinearLayoutManager(view.context)
+                    adapter = lessonsAdapter
+                }
+
+            }
         }
+        bind()
 
         val lessonRaw = Storage.getCurrentCourse()
         lessonRaw.observe(this.viewLifecycleOwner) {list ->
